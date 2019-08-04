de4e6b47333fbb52f8a2dca6783dd95abcce725f
sujitha <adhukurisujitha@gmail.com>
2019-08-04 10:16:55 +0530

    annotations

--- src/main/java/S.java
+++ src/main/java/S.java
@@ -1,2 +1,40 @@
+import org.testng.annotations.*;
+
 public class S {
+    @Test
+    public void testcase1(){
+        System.out.println("this is testcase1");
+    }
+    @BeforeSuite
+    public void beforesuite(){
+        System.out.println("execute before suite");
+    }
+    @BeforeTest
+    public void beforetest(){
+        System.out.println("execute before test");
+    }
+    @BeforeClass
+    public void beforeclass(){
+        System.out.println("execute beforeclass");
+    }
+    @BeforeMethod
+    public void beforemethod(){
+        System.out.println("execute beforemethod");
+    }
+    /*@AfterSuite
+    public void aftersuite(){
+        System.out.println("execute aftersuite");
+    }*/
+    @AfterTest
+    public void aftertest(){
+        System.out.println("exceute after test");
+    }
+    @AfterClass
+    public void afterclass(){
+        System.out.println("execute afterclass");
+    }
+    @AfterMethod
+    public void aftermethod(){
+        System.out.println("execute aftermethod");
+    }
 }
